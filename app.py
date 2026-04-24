17ca27f1ef49f9d70648ba240e033303d437d139
MMMJENHI <155253082+MMMJENHI@users.noreply.github.com>
2026-04-24 19:32:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
-from PIL import Image
-import os
 
-st.title("🦷 Test IA Dentaire")
+st.title("🦷 Diagnostic Système")
+st.write("Le serveur fonctionne !")
 
-# Test de présence du fichier
-if os.path.exists("dent.jpg"):
-    st.success("✅ Image 'dent.jpg' trouvée sur le serveur GitHub.")
-    img = Image.open("dent.jpg")
-    st.image(img)
-else:
-    st.error("❌ Fichier 'dent.jpg' absent du dépôt GitHub.")
+import cv2
+st.write(f"OpenCV est bien chargé (Version: {cv2.__version__})")
 
-st.write("Si vous voyez ce message, le serveur fonctionne !")
+import numpy as np
+st.write("Numpy est chargé.")
